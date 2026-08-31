a4c1f546ef8ad2d035eb128c62761bffa1f143b1
Andre Anzures <andreanzures0@gmail.com>
2026-08-30 23:39:33 -0600

    feat: se añadio la regeneracion en el combate | Deadpool_vs_Wolverine

--- Deadpool_vs_Wolverine.py
+++ Deadpool_vs_Wolverine.py
@@ -6,6 +6,7 @@ deadpool_vida = int(input("Ingresa la vida de Deadpool: "))
 wolverine_vida = int(input("Ingresa la vida de Wolverine: "))
 
 turno = 0
+regenerar = 0
 
 while(deadpool_vida and wolverine_vida > 0):
 
@@ -13,7 +14,11 @@ while(deadpool_vida and wolverine_vida > 0):
     print(f"\nTurno: {turno}")
 
     #Bloque de deadpool
-    if(random.random() > 0.2):
+    if(regenerar == 1):
+        print("Deadpool se esta regenerando...")
+        regenerar=0
+
+    elif(random.random() > 0.2):
 
         ataque_deadpool = random.randint(10,100)
         print(f"Deadpool ataca con {ataque_deadpool}")
@@ -21,6 +26,7 @@ while(deadpool_vida and wolverine_vida > 0):
 
         if(ataque_deadpool == 100):
             print("Deadpool dio un golpe max. Wolverine no atacara en el siguiente turno.")
+            regenerar=1
 
         wolverine_vida -=ataque_deadpool
 
@@ -30,17 +36,22 @@ while(deadpool_vida and wolverine_vida > 0):
             print(f"Vida restante de Wolverine; {wolverine_vida}")
         
     else:
-        print("Wolverine esquivo el ataque")
+        print("¡Wolverine esquivo el ataque!")
 
 
     #Bloque de wolverine
-    if(random.random() > 0.25):
+    if(regenerar == 1):
+        print("Wolverine se esta regenerando...")
+        regenerar=0
+
+    elif(random.random() > 0.25):
 
         ataque_wolverine = random.randint(10,120)
         print(f"Wolverine ataca con {ataque_wolverine}")
 
         if(ataque_wolverine == 120):
             print("Wolverine dio un golpe maximo. Deadpool no atacara en el siguiente turno.")
+            regenerar=1
 
         deadpool_vida -= ataque_wolverine
 
@@ -50,7 +61,7 @@ while(deadpool_vida and wolverine_vida > 0):
             print(f"Vida restante de Deadpool: {deadpool_vida}")
 
     else:
-        print("Deadpool esquivo el ataque")
+        print("¡Deadpool esquivo el ataque!")
 
     time.sleep(1)
 
